7e0c1bc996c7b5db934d62c25cd4020cd8c07d99
joshuasilva414 <joshuasilva414@gmail.com>
2024-11-02 15:54:29 -0500

    move app root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,3 @@
-import DrawSection from "@/components/draw-section";
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable";
-
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="w-screen h-screen">
-      <ResizablePanelGroup direction="horizontal">
-        <ResizablePanel>
-          <DrawSection />
-        </ResizablePanel>
-        <ResizableHandle />
-        <ResizablePanel>Two</ResizablePanel>
-      </ResizablePanelGroup>
-    </div>
-  );
+export default function Page() {
+  return <div></div>;
 }
