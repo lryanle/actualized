abf2161324bd011eb0032ab19ee9ccace001cb23
Liam Murray <liam@murray.to>
2024-11-03 02:09:06 -0600

    Add Jotai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-"use client"
+"use client";
 
 export default function Home() {
-  return (
-    <div className="flex justify-center items-center min-h-screen gap-16 w-screen h-[calc(100vh-100rem)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full h-full justify-center">
-
-      </main>
-    </div>
-  );
+	return (
+		<div className="flex justify-center items-center min-h-screen gap-16 w-screen h-[calc(100vh-100rem)]">
+			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full h-full justify-center"></main>
+		</div>
+	);
 }
-
